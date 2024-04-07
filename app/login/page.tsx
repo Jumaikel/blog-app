4b905d011f757957d298d74fe4fddec676f5a123
Jumaikel Chinchilla Mora <jumaikel.chinchilla.mora@est.una.ac.cr>
2024-04-06 18:42:58 -0600

    The necessary pages were created.

---
+++ app/login/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Main(){
+    return (
+        <main className="flex min-h-screen flex-col items-center bg-gradient-radial rounded-lg">
+      </main>
+
+    );
+}
---
+++ app/post/page.tsx
@@ -0,0 +1,12 @@
+import Footer from "../components/Footer";
+
+export default async function Home() {
+  return (
+    <>
+      <main className="flex min-h-screen flex-col items-center k p-5">
+        <h1 className="text-4xl font-bold text-turquoise p-5">Post</h1>
+      </main>
+      <Footer />
+    </>
+  );
+}
